8f9f9227a5106c39ecf97667ec9f635af92049f6
BroGames <hunempire11@gmail.com>
2025-01-10 09:01:53 +0100

    fixed generate.bat

--- generate.bat
+++ generate.bat
@@ -1,2 +1,14 @@
-cmake --build . --config Debug
-PAUSE
+set BUILD_DIR=sdl_build
+set CONFIG=Debug
+
+if not exist %BUILD_DIR% (
+    mkdir %BUILD_DIR%
+)
+
+cd %BUILD_DIR%
+
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Debug ../
+
+cmake --build . --config %CONFIG%
+
+pause
